02055eb47aee2f341b5377fefd427f8e2b24a7b3
DorianClark <dorian@klik2learn.com>
2020-04-29 01:01:58 +0100

    Chapter 6 complete
    Completed Chapter 6, including end exercise. Minor bug fixes throughout

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,8 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('index', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
